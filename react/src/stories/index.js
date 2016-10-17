95ca03eb821322f82a3fe42d0d2c7f8638864e6f
Hugo Agbonon <hugo@agbonon.fr>
2016-10-17 23:55:10 +0200

    wip

--- react/src/stories/index.js
+++ react/src/stories/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { storiesOf, action, linkTo } from '@kadira/storybook';
+import { storiesOf, action, describe, it, specs,
+beforeEach, before, after, afterEach, xdescribe,
+fit, xit } from "../../.storybook/facade";
+import expect from 'expect'
+import { create } from 'react-test-renderer';
+
 import Pokemon from '../components/Pokemon'
 import pokemons from '../data/pokemon'
 
@@ -7,6 +12,22 @@ require('../App.css')
 
 pokemons.map(pokemon => (
   storiesOf(pokemon.name, module)
-    .add('caught', () => <Pokemon {...pokemon} caught onSelect={action('clicked')} />)
-    .add('uncaught', () => <Pokemon {...pokemon} onSelect={action('clicked')}/>)
+    .add('caught', () => {
+      const story = <Pokemon {...pokemon} caught onSelect={action('clicked')} />
+      specs(() => describe(pokemon.name, () => (
+        it(`should render ${pokemon.name} caught`, () => {
+          expect(
+            create(
+              <Pokemon {...pokemon} />
+            )
+          ).toMatchSnapshot()
+        })
+      )))
+      return story
+    })
+
+    .add('uncaught', () => {
+      const story = <Pokemon {...pokemon} onSelect={action('clicked')}/>
+      return story
+    })
 ))
